f2ff7e21954e2b2aa66a56348574248878b97538
GafBoy <fear55@list.ru>
2020-07-30 23:06:08 +0300

    Задача 8

---
+++ src/Task8.java
@@ -0,0 +1,9 @@
+public class Task8 {
+    public static void main(String[] args) {
+        int num=1;
+        while(num<51){
+            System.out.println(num);
+            num++;
+        }
+    }
+}
