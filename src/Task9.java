cb535d7cd8695f6de477b41c8076312aab4816d3
GafBoy <fear55@list.ru>
2020-08-03 23:31:17 +0300

    Задача 9 Произведите ввод данных с клавиатуры в массив, а после этого произведите вывод массива на экран, где каждый элемент массива умножается на 2. Размер массива задается пользователем.

---
+++ src/Task9.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task9 {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введите размер массива: ");
+        int j = in.nextInt();
+        int mass[]=new int[j];
+
+        for( int i=0; i<j; i++){
+            mass[i]=in.nextInt();
+        }
+        for(int i=0; i<j; i++) {
+            System.out.println(mass[i]*2);
+        }
+    }
+}
