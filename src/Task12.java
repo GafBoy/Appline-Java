37b6a06f42465f7f8d1c57d1d4bc175b71bb5079
GafBoy <fear55@list.ru>
2020-08-10 23:17:57 +0300

    Задача 12 Для введенной с клавиатуры строки необходимо провести отсев пробелов. Результат в виде строки без пробелов вывести на экран.

---
+++ src/Task12.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Task12 {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String str= in.nextLine();
+
+        System.out.println(str.replace(" ", ""));
+
+    }
+}
