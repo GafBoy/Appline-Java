cd097ad6059787582e75cb73e12e498e868e439e
GafBoy <fear55@list.ru>
2020-08-14 23:40:05 +0300

    Задача 17 Необходимо написать программу, которая будет записывать текстовые данные, введенные с экрана, в файл .txt. Запись ввести в файл до тех пор, пока не будет введена команда стоп в консоли. Пользователь при тестировании программы по правилу черного ящика должен понимать, какой принцип остановки записи в файл он должен подать.

---
+++ src/Task17.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.io.*;
+
+public class Task17 {
+    public static void main(String[] args) {
+        String file;
+        BufferedReader txt = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Конец ввода строки 'стоп'");
+        try (FileWriter fw = new FileWriter("Task17.txt")) {
+            do {
+                System.out.print(": ");
+                file = txt.readLine();
+
+                if (file.compareTo("стоп") == 0) break;
+
+                file = file + "\r\n";
+                fw.write(file);
+            } while (file.compareTo("стоп") != 0);
+        } catch (IOException exc) {
+            System.out.println("Ошибка ввода-вывода " + exc);
+
+            }
+        }
+
+    }
